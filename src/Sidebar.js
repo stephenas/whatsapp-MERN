928d25f55066228b4ac1dc91ed26d079d8f84139
Steve <stevestephenas7@gmail.com>
2020-11-17 21:37:45 +0100

    Implemented Add new chat

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import SidebarChat from "./SidebarChat";
+import "./Sidebar.css";
+import db from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function Sidebar() {
+  const [rooms, setRooms] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) => {
+      setRooms(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar src="https://static.wixstatic.com/media/56d0b8_c17642ca06994bd3b4fe4b1c90adf644~mv2_d_3008_4011_s_4_2.jpeg/v1/crop/x_0,y_1043,w_2975,h_2968/fill/w_69,h_69,al_c,q_80,usm_0.66_1.00_0.01/56d0b8_c17642ca06994bd3b4fe4b1c90adf644~mv2_d_3008_4011_s_4_2.webp" />
+        <Avatar src={user?.photoURL} />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
@@ -27,6 +44,12 @@ function Sidebar() {
           <SearchOutlinedIcon />
           <input type="text" placeholder="Search or start new chat" />
         </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat addNewChat />
+        {rooms.map((room) => (
+          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+        ))}
       </div>
     </div>
   );
